ab6f8dc61b2236c36ec1f9ff1de73e5d4549babf
V1perW0lf <christopher.42.ellis@gmail.com>
2018-06-28 22:20:32 -0500

    Program.cs comments
    Updated comments

--- Program.cs
+++ Program.cs
@@ -15,11 +15,12 @@ namespace SHSU_ELO_Project
         static void Main()
         {
             SQLCode sql = new SQLCode();
-            // Update client version here
+            // Check for valid connection
             if(sql.isServerConnected() == false)
             {
                 MessageBox.Show("No connection to database.", "Elode", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
             }
+            // Check for latest version
             else if (sql.checkVersion() != 3)
             {
                 MessageBox.Show("Your client is not the latest version. Please update to the latest version of the software.", "Elode", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
